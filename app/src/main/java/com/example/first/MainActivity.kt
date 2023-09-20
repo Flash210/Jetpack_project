84116903f0a3f6aa491429fceb4922da9edd9139
houcem <houssemltaif77]@gmail.com>
2023-09-20 19:54:19 +0100

    login screen

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -3,13 +3,43 @@ package com.example.first
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.first.ui.theme.FirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +52,135 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    login()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun login() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(
+                color = Color(android.graphics.Color.parseColor("#ffffff"))
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.top_background),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo Image",
+            modifier = Modifier.height(150.dp)
+        )
+
+        Text(
+            "Welcome to App ",
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            color = Color(android.graphics.Color.parseColor("#7d32a8"))
+        )
+
+        var user by remember {
+            mutableStateOf("UserName")
+        }
+
+        var pass by remember {
+            mutableStateOf("")
+        }
+
+        var passVisibility by remember {
+            mutableStateOf(false)
+        }
+
+
+        TextField(
+            value = user,
+            { text ->
+                user = text
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(
+                    start = 66.dp,
+                    end = 66.dp,
+                    bottom = 7.dp,
+                    top = 7.dp
+                )
+                .border(
+                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                fontSize = 14.sp
+            ),
+
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+
+
+            )
+
+
+
+
+        TextField(
+            value = pass,
+            { text ->
+                pass = text
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(
+                    start = 66.dp,
+                    end = 66.dp,
+                    bottom = 7.dp,
+                    top = 7.dp
+                )
+                .border(
+                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                fontSize = 14.sp
+            ),
+
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            visualTransformation = if (passVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+
+
+        )
+
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstTheme {
-        Greeting("Android")
-    }
-}
