4faa4de4ed2f8d71e3a3c3f2422625260384611e
houcem <houssemltaif77]@gmail.com>
2023-09-20 21:08:19 +0100

    final touch

---
+++ app/src/main/java/com/example/first/ui/theme/details/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.first.ui.theme.details
+
+import androidx.compose.ui.graphics.Color
+
+val  purpleColor = Color(0xFF7D32A8)
--- app/src/main/java/com/example/first/ui/theme/screens/LoginScreen.kt
+++ app/src/main/java/com/example/first/ui/theme/screens/LoginScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -35,14 +38,29 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.first.R
+import com.example.first.ui.theme.details.purpleColor
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
 fun LoginScreen(
-     navController: NavController
+    navController: NavController
 ) {
+
+    var user by remember {
+        mutableStateOf("UserName")
+    }
+
+    var pass by remember {
+        mutableStateOf("password")
+    }
+
+    var passVisibility by remember {
+        mutableStateOf(false)
+    }
+
+
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -72,17 +90,6 @@ fun LoginScreen(
             color = Color(android.graphics.Color.parseColor("#7d32a8"))
         )
 
-        var user by remember {
-            mutableStateOf("UserName")
-        }
-
-        var pass by remember {
-            mutableStateOf("")
-        }
-
-        var passVisibility by remember {
-            mutableStateOf(false)
-        }
 
 
         TextField(
@@ -100,13 +107,14 @@ fun LoginScreen(
                     top = 7.dp
                 )
                 .border(
-                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    1.dp,
+                    color = purpleColor,
                     shape = RoundedCornerShape(50)
                 ),
             shape = RoundedCornerShape(50),
             textStyle = TextStyle(
                 textAlign = TextAlign.Center,
-                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                color = purpleColor,
                 fontSize = 14.sp
             ),
 
@@ -137,13 +145,13 @@ fun LoginScreen(
                     top = 7.dp
                 )
                 .border(
-                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    1.dp, color = purpleColor,
                     shape = RoundedCornerShape(50)
                 ),
             shape = RoundedCornerShape(50),
             textStyle = TextStyle(
                 textAlign = TextAlign.Center,
-                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                color = purpleColor,
                 fontSize = 14.sp
             ),
 
@@ -156,6 +164,68 @@ fun LoginScreen(
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
 
 
-        )}}
+        )
+
+
+        Button(
+            onClick = { /*TODO*/ },
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(
+                    start = 66.dp,
+                    end = 66.dp,
+                    bottom = 7.dp,
+                    top = 7.dp
+                ),
+            colors = ButtonDefaults.buttonColors(
+
+                //user purpleColor from details/Constants.kt
+                containerColor = purpleColor
+            ),
+            shape = RoundedCornerShape(50)
+        ) {
+
+            Text(
+                text = "Login",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+
+                )
+        }
+        Text(text = "Don't have an account? Sign Up",
+            color = purpleColor,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(top=7.dp, bottom = 7.dp))
+
+        Row (
+
+        ){
+            Image(painter = painterResource(id = R.drawable.google),
+                contentDescription ="" ,
+                Modifier. padding(7.dp)
+                )
+
+            Image(painter = painterResource(id = R.drawable.facebook),
+                contentDescription ="" ,
+                Modifier.padding(7.dp)
+            )
+            Image(painter = painterResource(id = R.drawable.twitter),
+                contentDescription ="" ,
+                Modifier.padding(7.dp)
+            )
+
+        }
+
+        Image(painterResource(id = R.drawable.bottom_background)  ,
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+            )
+
+
+    }
+}
 
 
